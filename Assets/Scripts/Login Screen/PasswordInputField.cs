78dbdce1d2a248bbf8ee759a488b16c523291b38
Ali Saneei <realalisaneei@gmail.com>
2025-01-27 12:06:50 -0500

    Fix the input field and implement the shutdown button
    Thanks Gleb for your help! :)

--- Assets/Scripts/Login Screen/PasswordInputField.cs
+++ Assets/Scripts/Login Screen/PasswordInputField.cs
@@ -1,19 +1,25 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Text.RegularExpressions;
 
 public class PasswordInputField : MonoBehaviour
 {
-    private string password; 
+    [SerializeField] private TMP_InputField passwordPrompt;
 
-    public void GrabFromInputField (string input)
-    {
-        password = input;
+    public void GrabFromInputField() {
+        string input = passwordPrompt.text;
+        input = Regex.Replace(input, @"\s+", String.Empty);
 
-        DisplayReactiontoPassword();
+        if (!String.IsNullOrEmpty(input)) {
+            DisplayReactiontoPassword();
+        } else {
+            Debug.LogWarning("PASSWORD INPUT CANNOT BE EMPTY");
+        }
     }
 
-    public void DisplayReactiontoPassword()
-    {
+    public void DisplayReactiontoPassword() {
         Debug.Log("Welcome!");
 
         SceneManager.LoadScene("Desktop");
