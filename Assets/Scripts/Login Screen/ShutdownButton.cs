daafffbe5ba0a0fd8de49d3fa592e75c945f94bf
Ali Saneei <realalisaneei@gmail.com>
2025-02-03 20:20:31 -0500

    Implement the music, startup, and shutdown sounds

--- Assets/Scripts/Login Screen/ShutdownButton.cs
+++ Assets/Scripts/Login Screen/ShutdownButton.cs
@@ -1,9 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShutdownButton : MonoBehaviour
 {
+    public Animator panelAnimator;
+
+    public void PlayExitStuff()
+    {
+        panelAnimator.Play("Login Screen Fade Out");
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+
+        audioSource.Play();
+
+        Invoke("QuitTheGame", 2);
+    }
+
     public void QuitTheGame()
     {
         Application.Quit();
