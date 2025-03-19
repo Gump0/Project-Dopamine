0b684b7a0f2692f39e3f143a137b65cbbfb084b8
Ali Saneei <realalisaneei@gmail.com>
2025-03-19 09:10:09 -0400

    Fixed several problems with the End Screen

---
+++ Assets/EndScreenButtons.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EndScreenButtons : MonoBehaviour
+{
+    public void StartANewGame()
+    {
+
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
